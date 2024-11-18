52d60c137caf32b00d5c969724f8a452d490d181
Tian <jacky841224j@gmail.com>
2024-11-18 23:59:37 +0800

    feat:增加model

---
+++ Model/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TGBot_TW_Stock_Webhook.Model
+{
+    /// <summary>
+    /// 訂閱清單
+    /// </summary>
+    public class Subscription
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+
+        public string Stock { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,22 @@
+﻿namespace TGBot_TW_Stock_Webhook.Model
+{
+    /// <summary>
+    /// 使用者
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 是否開啟訂閱
+        /// </summary>
+        public bool IsSubActive { get; set; }
+
+        /// <summary>
+        /// 是否停用帳號
+        /// </summary>
+        public bool IsDelete { get; set; }
+
+        public ICollection<Subscription> Subscription { get; set; } = [];
+    }
+}
