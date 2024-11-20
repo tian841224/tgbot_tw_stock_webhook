b2cc88c8b14b3717d8f1c9773a783e0e4fd3c9fd
Tian <jacky841224j@gmail.com>
2024-11-20 20:17:31 +0800

    modify:調整User 欄位

--- Model/Entities/User.cs
+++ Model/Entities/User.cs
@@ -15,7 +15,12 @@
         /// <summary>
         /// 是否開啟訂閱
         /// </summary>
-        public bool IsSubActive { get; set; }
+        public bool SubActive { get; set; }
+
+        /// <summary>
+        /// 是否被停用
+        /// </summary>
+        public bool Status { get; set; }
 
         public ICollection<Subscription> Subscription { get; set; } = [];
     }
