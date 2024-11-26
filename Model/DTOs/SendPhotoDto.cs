d719f2a8b9bad6d5b02c5be15a6b8c557d11b6b5
Tian <jacky841224j@gmail.com>
2024-11-26 17:30:23 +0800

    feat:抽離Bot SendPhoto方法

---
+++ Model/DTOs/SendPhotoDto.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot.Types;
+
+namespace TGBot_TW_Stock_Webhook.Model.DTOs
+{
+    public class SendPhotoDto
+    {
+        public required Message Message { get; set; }
+
+        public required InputFile Photo { get; set; }
+
+        public string? Caption { get; set; } = null;
+
+        public CancellationToken CancellationToken { get; set; }
+    }
+}
