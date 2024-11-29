8be5b3d20c3e139df211c2ecb99d0cc4d8f56afd
Tian <jacky841224j@gmail.com>
2024-11-29 20:57:12 +0800

    feat:新增SubscriptionRepository、UserRepository

---
+++ Interface/Repository/ISubscriptionRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TGBot_TW_Stock_Webhook.Model.Entities;
+
+namespace TGBot_TW_Stock_Webhook.Interface
+{
+    public interface ISubscriptionRepository
+    {
+        /// <summary> 使用ID取得Subscription </summary>
+        Task<Subscription?> GetById(int id);
+        /// <summary> 使用UserId取得Subscription </summary>
+        Task<List<Subscription>?> GetByUserId(int userId);
+
+        /// <summary> 取得全部Subscription </summary>
+        Task<List<Subscription>> GetAll();
+    }
+}
---
+++ Interface/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+using TGBot_TW_Stock_Webhook.Model.Entities;
+
+namespace TGBot_TW_Stock_Webhook.Interface
+{
+    public interface IUserRepository
+    {
+        /// <summary> 使用ID取得User </summary>
+        Task<User?> GetById(int id);
+
+        /// <summary> 取得全部User </summary>
+        Task<List<User>> GetAll();
+    }
+}
