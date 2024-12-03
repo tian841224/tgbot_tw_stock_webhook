5c54c7ea3d344a31ef07d170f6f3acc204a1d2ef
Tian <tian@stellarsol.info>
2024-12-03 16:37:37 +0800

    modify:增加SubscriptionItemEnum代號

--- Enum/SubscriptionItemEnum.cs
+++ Enum/SubscriptionItemEnum.cs
@@ -3,15 +3,15 @@ namespace TGBot_TW_Stock_Webhook.Enum
     public enum SubscriptionItemEnum
     {
         /// <summary> 個股資訊 </summary>
-        StockInfo,
+        StockInfo = 1,
 
         /// <summary> 個股新聞 </summary>
-        StockNews,
+        StockNews = 2,
 
         /// <summary> 當日市場成交資訊 </summary>
-        DailyMarketInfo,
+        DailyMarketInfo = 3,
 
         /// <summary> 成交量前20股票 </summary>
-        TopVolumeItems,
+        TopVolumeItems = 4,
     }
 }
