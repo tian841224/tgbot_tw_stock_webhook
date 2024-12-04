0bc84acc953e8e297c07a3a1fce409e06952d810
Tian <jacky841224j@gmail.com>
2024-12-04 21:43:49 +0800

    feat:新增NotificationController、調整INotificationService檔案位置

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TGBot_TW_Stock_Webhook.Interface;
+
+namespace TGBot_TW_Stock_Webhook.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class NotificationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> SendDailyMarketInfoAsync([FromServices] INotificationService notificationService, CancellationToken ct)
+        {
+            try
+            {
+                await notificationService.SendDailyMarketInfoAsync(ct);
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendStockInfoAsync([FromServices] INotificationService notificationService, CancellationToken ct)
+        {
+            try
+            {
+                await notificationService.SendStockInfoAsync(ct);
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendStockNewsAsync([FromServices] INotificationService notificationService, CancellationToken ct)
+        {
+            try
+            {
+                await notificationService.SendStockNewsAsync(ct);
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendTopVolumeItemsAsync([FromServices] INotificationService notificationService, CancellationToken ct)
+        {
+            try
+            {
+                await notificationService.SendTopVolumeItemsAsync(ct);
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception);
+            }
+        }
+    }
+}
