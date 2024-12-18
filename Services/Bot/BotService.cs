b3ebbec680c11746bb43cd062e7411c998b94df4
Tian <jacky841224j@gmail.com>
2024-12-18 16:14:17 +0800

    modify:BotService加入try catch

--- Services/Bot/BotService.cs
+++ Services/Bot/BotService.cs
@@ -1,4 +1,5 @@
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -7,87 +8,147 @@ using TGBot_TW_Stock_Webhook.Model.DTOs;
 
 namespace TGBot_TW_Stock_Webhook.Services.Bot
 {
-    public class BotService(ITelegramBotClient _botClient) : IBotService
+    public class BotService(ILogger<NotificationService> _logger,ITelegramBotClient _botClient) : IBotService
     {
         public async Task<Message> SendTextMessageAsync(SendTextDto dto)
         {
-            dto.CancellationToken.ThrowIfCancellationRequested();
-            return await _botClient.SendMessage(
-                chatId: dto.Message.Chat.Id,
-                text: dto.Text,
-                replyMarkup: dto.ReplyMarkup,
-                parseMode: dto.ParseMode ?? ParseMode.Html,
-                cancellationToken: dto.CancellationToken);
+            try
+            {
+                dto.CancellationToken.ThrowIfCancellationRequested();
+                return await _botClient.SendMessage(
+                    chatId: dto.Message.Chat.Id,
+                    text: dto.Text,
+                    replyMarkup: dto.ReplyMarkup,
+                    parseMode: dto.ParseMode ?? ParseMode.Html,
+                    cancellationToken: dto.CancellationToken);
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendTextMessageAsync");
+            }
+
+            return new Message();
         }
 
         public async Task<Message> SendPhotoAsync(SendPhotoDto dto)
         {
-            dto.CancellationToken.ThrowIfCancellationRequested();
+            try
+            {
+                dto.CancellationToken.ThrowIfCancellationRequested();
+                return await _botClient.SendPhoto(
+                    caption: dto.Caption,
+                    chatId: dto.Message.Chat.Id,
+                    photo: dto.Photo,
+                    parseMode: ParseMode.Html,
+                    cancellationToken: dto.CancellationToken);
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendPhotoAsync");
+            }
 
-            return await _botClient.SendPhoto(
-                caption: dto.Caption,
-                chatId: dto.Message.Chat.Id,
-                photo: dto.Photo,
-                parseMode: ParseMode.Html,
-                cancellationToken: dto.CancellationToken);
+            return new Message();
         }
 
         public async Task<Message> SendHelloMessageAsync(Message message, CancellationToken cancellationToken)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            return await SendTextMessageAsync(new SendTextDto
+            try
             {
-                Message = message,
-                Text = $"Hello {message.Chat.FirstName} {message.Chat.LastName}",
-                ParseMode = ParseMode.Html,
-                CancellationToken = cancellationToken
-            });
+                cancellationToken.ThrowIfCancellationRequested();
+                return await SendTextMessageAsync(new SendTextDto
+                {
+                    Message = message,
+                    Text = $"Hello {message.Chat.FirstName} {message.Chat.LastName}",
+                    ParseMode = ParseMode.Html,
+                    CancellationToken = cancellationToken
+                });
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendPhotoAsync");
+            }
+
+            return new Message();
         }
 
         public async Task SendErrorMessageAsync(Message message, CancellationToken cancellationToken)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            await SendTextMessageAsync(new SendTextDto
+            try
             {
-                Message = message,
-                Text = "指令錯誤請重新輸入",
-                ParseMode = ParseMode.Html,
-                CancellationToken = cancellationToken
-            });
+                cancellationToken.ThrowIfCancellationRequested();
+                await SendTextMessageAsync(new SendTextDto
+                {
+                    Message = message,
+                    Text = "指令錯誤請重新輸入",
+                    ParseMode = ParseMode.Html,
+                    CancellationToken = cancellationToken
+                });
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendPhotoAsync");
+            }
+
+            return;
         }
 
         public async Task<Message> SendWaitMessageAsync(Message message, CancellationToken cancellationToken)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            return await SendTextMessageAsync(new SendTextDto
+            try
             {
-                Message = message,
-                Text = $"<b>-讀取中，請稍後⏰-</b>",
-                ReplyMarkup = new ReplyKeyboardRemove(),
-                ParseMode = ParseMode.Html,
-                CancellationToken = cancellationToken
-            });
+                cancellationToken.ThrowIfCancellationRequested();
+                return await SendTextMessageAsync(new SendTextDto
+                {
+                    Message = message,
+                    Text = $"<b>-讀取中，請稍後⏰-</b>",
+                    ReplyMarkup = new ReplyKeyboardRemove(),
+                    ParseMode = ParseMode.Html,
+                    CancellationToken = cancellationToken
+                });
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendPhotoAsync");
+            }
+
+            return new Message();
         }
 
         public async Task DeleteMessageAsync(DeleteMessageDto dto)
         {
-            dto.CancellationToken.ThrowIfCancellationRequested();
-            await _botClient.DeleteMessage(chatId: dto.Message.Chat.Id,
-                                           messageId: dto.Reply.MessageId,
-                                           cancellationToken: dto.CancellationToken);
+            try
+            {
+                dto.CancellationToken.ThrowIfCancellationRequested();
+                await _botClient.DeleteMessage(chatId: dto.Message.Chat.Id,
+                                               messageId: dto.Reply.MessageId,
+                                               cancellationToken: dto.CancellationToken);
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendPhotoAsync");
+            }
+            return ;
         }
 
 
         public async Task ErrorNotify(Message message, string errorMessage, CancellationToken cancellationToken)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            await _botClient.SendMessage(
-                text: $"UserId：{message.Chat.Id}\n" +
-                $"Username：{message.Chat.Username}\n" +
-                $"錯誤：\n {errorMessage}",
-                chatId: 806077724,
-                parseMode: ParseMode.Html,
-                cancellationToken: cancellationToken);
+            try
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                await _botClient.SendMessage(
+                    text: $"UserId：{message.Chat.Id}\n" +
+                    $"Username：{message.Chat.Username}\n" +
+                    $"錯誤：\n {errorMessage}",
+                    chatId: 806077724,
+                    parseMode: ParseMode.Html,
+                    cancellationToken: cancellationToken);
+            }
+            catch (ApiRequestException ex)
+            {
+                _logger.LogError(ex.Message, "SendPhotoAsync");
+            }
+            return;
         }
     }
 }
